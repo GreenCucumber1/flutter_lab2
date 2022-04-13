90020fb99f591e75e7c7f13f3c91b408edc0a980
GreenCucumber1 <pashateplov74@yandex.ru>
2022-04-13 21:55:00 +0300

    first

---
+++ lib/my_home_page1.dart
@@ -0,0 +1,119 @@
+
+
+
+import 'my_home_page2.dart';
+import 'package:flutter/material.dart';
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageFromState();
+}
+
+class _MyHomePageFromState extends State<MyHomePage> {
+  final _controller = TextEditingController();
+  final _controller1 = TextEditingController();
+  bool texterror = false;
+
+  @override
+  void initState() {
+    _controller.text = "";
+    _controller1.text = "";
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        title: const Text("WeatherBit API"),
+      ),
+      body: Center (
+        
+        child: Container(
+          margin: const EdgeInsets.all(15.0),
+          color: Color.fromARGB(255, 218, 233, 233),
+          width: 1200,
+          height: 500,
+          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+                         
+              const Padding(
+                padding: EdgeInsets.only(bottom: 20),),
+              const Padding(
+                padding: EdgeInsets.all(8),
+                child: Text('Print lat and lon',
+                    style: TextStyle(fontSize: 25, color: Colors.black)),
+              ),
+              
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: SizedBox(                      
+                  width: 50,
+                  child: TextFormField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      labelText: 'Lan',
+                      errorText: texterror ? 'Print correct URL' : null,
+                    ),
+                  ),
+                ),
+              ),
+
+                Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: SizedBox(                      
+                  width: 50,
+                  child: TextFormField(
+                    controller: _controller1,
+                      decoration: InputDecoration(
+                      labelText: 'Lon',
+                      errorText: texterror ? 'Print correct URL' : null,
+                    ),
+                  ),
+                ),
+              ),
+              
+
+
+
+              const SizedBox(height: 25),
+              ElevatedButton(
+                onPressed: () {
+                  if (_controller.text.isEmpty && _controller1.text.isEmpty  ) {
+                    setState(() {
+                      texterror = true;
+                    });
+                  } else {
+                    setState(() {
+                      texterror = false;
+                    });
+                    String url = _controller.text;
+                    String url1 = _controller1.text;
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SecondPage(url: url, url1:url1),
+                        
+                      ),
+                    );
+                    _controller.clear();
+                    _controller1.clear();
+                  }
+                },
+                child: const Text("Check"),
+                
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/my_home_page2.dart
@@ -0,0 +1,110 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+
+
+
+Future<WeatherHTTP> fetchFanFromServer(String x,String y) async{
+  String qwer="https://api.weatherbit.io/v2.0/forecast/daily?lat=${x}&lon=${y}&key=5e2805de11cd4b7baad01f51ed9c7a23&include=minutely";
+  final response = await http.get(Uri.parse(qwer));
+    return WeatherHTTP.fromJson(jsonDecode(response.body));
+}
+
+class WeatherHTTP {
+
+  final String city_name ;
+  final String state_code ;
+  final String country_code ;
+  final String timezone ;
+  final double lat ;
+  final double lon ;
+  final List<dynamic> data;
+
+  
+
+const WeatherHTTP({ 
+  required this.city_name,
+  required this.state_code,
+  required this.country_code,
+  required this.timezone,
+  required this.data,
+  required this.lat,
+  required this.lon,
+  });
+  
+  factory WeatherHTTP.fromJson(Map<String,dynamic> json) {
+    return WeatherHTTP(
+      city_name: json['city_name'],
+      state_code: json['state_code'],
+      country_code: json['country_code'],
+      timezone: json['timezone'],
+      data: json['data'],
+      lat: json['lat'],
+      lon: json['lon'],
+      
+    );
+    
+  }
+}
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({
+    required this.url,
+    required this.url1,
+    Key? key,}) : super(key: key);
+
+    final String url;
+    final String url1;
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  final _formKey = GlobalKey<FormState>();
+  late Future<WeatherHTTP> futureFanFromServer;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _formKey,
+      appBar: AppBar(
+        title: Text('Back'),
+      ),
+        body: Center(
+          child: FutureBuilder<WeatherHTTP>(
+            future: fetchFanFromServer(widget.url,widget.url1),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                
+               return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    
+                    Text("City name: " + snapshot.data!.city_name,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17.0),),
+                    Text("State code: " + snapshot.data!.state_code,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17.0),),
+                    Text("Country code: " + snapshot.data!.country_code,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17.0),),
+                    Text("Lat: " + snapshot.data!.lat.toString(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17.0),),
+                    Text("Lon: " + snapshot.data!.lon.toString(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17.0),),
+                    Text("Timezone: " + snapshot.data!.timezone,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17.0),),
+                    Text("And other data:",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17.0),),
+                    Text(snapshot.data!.data[2].toString(),textAlign: TextAlign.center),
+                    
+                    
+                  ],  
+                );
+
+                    
+                
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      
+    );
+  }
+}
